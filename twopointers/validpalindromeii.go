c492d55b8af8be3fcdf5deceb141b47cad639ab2
Kreagentle <ponwrt@gmail.com>
2024-12-25 23:25:53 +0000

    Valide palindrome ii to cover all cases

--- twopointers/validpalindromeii.go
+++ twopointers/validpalindromeii.go
@@ -1,35 +1,27 @@
 package twopointers
 
 func isPalindromeii(str string) bool {
-	if len(str) < 3 {
-		return true
-	}
 	var left, right int
-	temp := false
 	right = len(str) - 1
-	if len(str) != 3 {
-		for left <= right {
-			if str[left] != str[right] {
-				if temp == false {
-					temp = true
-					if str[left+1] == str[right] && str[left] != str[right-1] {
-						left += 1
-					} else {
-						right -= 1
-					}
-				} else {
-					return false
-				}
-			}
-			right -= 1
-			left += 1
+	for left <= right {
+		if str[left] != str[right] {
+			return isPal(str[left+1:right+1]) || isPal(str[left:right])
 		}
-	} else {
-		for left <= right {
-			if str[left] != str[right] {
-				return false
-			}
+		right -= 1
+		left += 1
+	}
+	return true
+}
+
+func isPal(str string) bool {
+	var i, j int
+	j = len(str) - 1
+	for i <= j {
+		if str[i] != str[j] {
+			return false
 		}
+		j -= 1
+		i += 1
 	}
 	return true
 }
