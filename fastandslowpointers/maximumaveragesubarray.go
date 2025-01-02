4843517a1b24a16e999f20df340cae69f454d746
Kreagentle <41452152+Kreagentle@users.noreply.github.com>
2025-01-02 23:24:51 +0000

    Create maximumaveragesubarray.go

---
+++ fastandslowpointers/maximumaveragesubarray.go
@@ -0,0 +1,16 @@
+package main
+
+func findMaxAverage(nums []int, k int) float64 {
+  var currentsum, maxsum int
+  for i := 0; i < k; i++ {
+    currentsum += nums[i]
+  }
+  maxsum = currentsum
+  for i, j := 0, k; j < len(nums); i, j = i+1,j+1 {
+    currentsum = currentsum - nums[i] + nums[j]
+    if currentsum > maxsum {
+      maxsum = currentsum
+    }
+  }
+	return float64(maxsum)/float64(k)
+}
