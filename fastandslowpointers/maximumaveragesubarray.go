1726394c12cb50d830bdb6be7e8e8ab9999cb45a
Kreagentle <ponwrt@gmail.com>
2025-01-04 23:55:13 +0000

    Rename packages

--- fastandslowpointers/maximumaveragesubarray.go
+++ fastandslowpointers/maximumaveragesubarray.go
@@ -1,16 +1,16 @@
-package main
+package fastandslowpointers
 
 func findMaxAverage(nums []int, k int) float64 {
-  var currentsum, maxsum int
-  for i := 0; i < k; i++ {
-    currentsum += nums[i]
-  }
-  maxsum = currentsum
-  for i, j := 0, k; j < len(nums); i, j = i+1,j+1 {
-    currentsum = currentsum - nums[i] + nums[j]
-    if currentsum > maxsum {
-      maxsum = currentsum
-    }
-  }
-	return float64(maxsum)/float64(k)
+	var currentsum, maxsum int
+	for i := 0; i < k; i++ {
+		currentsum += nums[i]
+	}
+	maxsum = currentsum
+	for i, j := 0, k; j < len(nums); i, j = i+1, j+1 {
+		currentsum = currentsum - nums[i] + nums[j]
+		if currentsum > maxsum {
+			maxsum = currentsum
+		}
+	}
+	return float64(maxsum) / float64(k)
 }
